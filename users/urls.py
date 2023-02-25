28e51d234e9a2c8f303e6e74eb5c207d86b2291b
udarakanishka <udara@octopusbi.com>
2023-02-25 22:26:22 +0530

    ad

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -60,3 +62,6 @@ urlpatterns = [
 
 
 ] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

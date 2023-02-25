8955a2e5723d639616fa2c1030393bf53af591ac
udarakanishka <udara@octopusbi.com>
2023-02-25 22:18:52 +0530

    static url

--- core/urls.py
+++ core/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('api/v1/cards/', include('cards.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
